0b96d05965e199e4d9637f9e8f0ab97547e99a6c
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2021-10-22 23:11:01 -0400

    Prospector Update 1
    Added Layout Script

---
+++ Assets/Prospector/__Scripts/Layout.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//the SlotDef class is not a subclass of MonoBehavior, so it doesn't need a seperate C# script
+
+[System.Serialiazable] // this makes SlotDefs visible in the Unity Inspector pane
+public class SlotDef {
+    public float x;
+    public float y;
+    public bool faceUp = false;
+    public string layerName = "Default";
+    public int layerID = 0;
+    public int id;
+    public List<int> hiddenBy = new List<int>();
+    public string type = "slot";
+    public Vector2 stagger;
+}
+
+public class Layout : MonoBehaviour {
+    public PT_XMLReader xmlr; // just like deck, this has a PT_XMLReader
+    public PT_XMLHashtable xml; // this variable is for faster xml access
+    public Vector2 multiplier; //the offset of the tableau's center
+
+    // SlotDef references
+    public List<SlotDef> slotDefs; //All the SlotDefs for Row0 - Row3
+    public SlotDef drawPile; 
+    public SlotDef discardPile;
+
+    //This holds all the possible names for the layers set by layerID
+    public string[] sortingLayerNames = new string[] {"Row0", "Row1", "Row2", "Row3", "Discard", "Draw"};
+
+    public void ReadLayout(string xmlText) {
+        xmlr = new PT_XMLReader();
+        xmlr.Parse(xmlText); //the XML is parsed
+        xml = xmlr.xml["xmlr"][0]; //amd xml is set as a shortcut to the XML
+
+        // read in the multiplier, which sets card spacing
+        multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
+        multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
+
+        //read in the slots
+        SlotDef tSD;
+        //slotsX is used as a shortcut to all the <slot>s
+        PT_XMLHashList slotsX = xml["slot"];
+
+        for (int i = 0; i < slotsX.Count; i++) {
+            tSD = new SlotDef(); //Create a new SlotDef instance
+            if (slotsX[i].HasAtt("type")) {
+                //if this <slot> has a type attribute parse it
+                tSD.type = slotsX[i].att("type");
+            } else {
+                //if not, set its tyoe to "slot"; its a card in the rows
+                tSD.type = "slot";
+            }
+
+            //Various attributes are parsed into numerical values
+            tSD.x = float.Parse(slotsX[i],att("x"));
+            tSD.y = float.Parse(slotsX[i],att("y"));
+            tSD.layerID = int.Parse(slotsX[i].att("layer"));
+            //this converts the number of the LayerID into the text layerName
+            tSD.layerName = sortingLayerNames[tSD.layerID];
+
+            switch(tSD.type) {
+                //pull additional attributes based on the tyoe of this <slot>
+                case "slot":
+                    tSD.faceUp = (slotsX[i].att("faceup") == "1");
+                    tSD.id = int.Parse(slotsX[i].att("id"));
+                    if (slotsX[i].HasAtt("hiddenby")) {
+                        string[] hiding = slotsX.att("hiddenby").Split(',');
+                        foreach (string s in hiding) {
+                            tSD.hiddenBy.Add(int.Parse(s));
+                        }
+                    }
+                    SlotDefs.Add(tSD);
+                    break;
+
+                case "drawpile":
+                    tSD.stagger.x = float.Parse(slotsX[i].att("xstagger"));
+                    drawPile = tSD;
+                    break;
+
+                case "discardpile":
+                    discardPile = tSD;
+                    break;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
